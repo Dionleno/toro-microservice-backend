05b09a4d4ff2b38d63894f2b4cf6dcfe4b2e9ce8
Dionleno <jhonsp_leno@hotmail.com>
2023-10-26 18:17:15 -0300

    feat: adding serverless webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-var-requires */ 
+const path = require('path')
+const slsw = require('serverless-webpack')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+const { isLocal } = slsw.lib.webpack
+const nodeExternals = require('webpack-node-externals')
+require('source-map-support').install()
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  mode: isLocal ? 'development' : 'production',
+  devtool: isLocal ? 'source-map' : 'eval',
+  externals: [nodeExternals()],
+  optimization: {
+    nodeEnv: false,
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.ts'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  plugins: [
+
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+}
